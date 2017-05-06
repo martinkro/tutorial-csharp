c8d810ddae36d079c9897541c645d6d516faab73
sundayliu <martinkro@aliyun.com>
2017-05-06 21:11:04 +0800

    Simple MVVM

---
+++ tutorials/HelloWPF/StringData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+
+    public class StringData
+    {
+        ObservableCollection<string> lst = new ObservableCollection<string>();
+        public StringData()
+        {
+            lst.Add("Abhishek");
+            lst.Add("Abhijit");
+            lst.Add("Kunal");
+            lst.Add("Sheo");
+        }
+
+        public ObservableCollection<string> GetStrings()
+        {
+            return lst;
+        }
+    }
+
