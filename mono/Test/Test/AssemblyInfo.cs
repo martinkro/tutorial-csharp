af86e89eeded35ae6b6fd0610fc1aa1649747bbc
sundayliu <martinkro@aliyun.com>
2017-09-01 16:01:38 +0800

    test

---
+++ mono/Test/Test/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("This is a simple description")]
+[assembly: AssemblyConfiguration("This is a simple configration")]
+[assembly: AssemblyCompany("Company")]
+[assembly: AssemblyProduct("Product")]
+[assembly: AssemblyCopyright("Personal")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("English")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("2.0.5.33")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
